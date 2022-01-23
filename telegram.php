13915c252c33a9f26da82dd1f9b69d00911f8a49
fozilbekNorkulov <98279244+fozilbekNorkulov@users.noreply.github.com>
2022-01-23 23:06:10 +0500

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5250852815:AAF-tOTnoZUzQw0kRCHmQefnO-pjkK5R-IQ/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5250852815:AAF-tOTnoZUzQw0kRCHmQefnO-pjkK5R-IQ";
+$chat_id = "-790321153";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
